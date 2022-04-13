329af8b6afb6094660e930754d8e50996de337fd
KDY <rlaeodud9983@naver.com>
2022-04-13 21:06:17 +0900

    [UPDATE] 파일명 변경

--- app/src/main/java/org/software/githubapiproject/data/SearchUserInfo.kt
+++ app/src/main/java/org/software/githubapiproject/data/SearchUserInfo.kt
@@ -1,6 +1,6 @@
 package org.software.githubapiproject.data
 
-data class UserInfo(
+data class SearchUserInfo(
     var incomplete_results: Boolean,
     var items: List<Item>,
     var total_count: Int
