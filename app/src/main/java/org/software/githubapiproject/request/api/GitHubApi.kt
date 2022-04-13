639e052136121ae2f73012653c942ec2de0ff203
KDY <rlaeodud9983@naver.com>
2022-04-13 21:05:28 +0900

    [ADD] GitHub 유저 정보를 가져오기 위한 Api 추가

--- app/src/main/java/org/software/githubapiproject/request/api/GitHubApi.kt
+++ app/src/main/java/org/software/githubapiproject/request/api/GitHubApi.kt
@@ -1,14 +1,22 @@
 package org.software.githubapiproject.request.api
 
+import org.software.githubapiproject.data.SearchUserInfo
 import org.software.githubapiproject.data.UserInfo
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GitHubApi {
     @GET("/search/users")
-    suspend fun getUser(
+    suspend fun getSearchResponse(
         @Query("q") login: String?,
         @Query("page") page : Int = 1,
+    ):Response<SearchUserInfo>
+
+    @GET("/users/{userId}")
+    suspend fun getUserInfo(
+        @Path("userId") userId : String?
     ):Response<UserInfo>
+
 }
