e990489260d58875e3e9fbb67ecc41ca07322581
KDY <rlaeodud9983@naver.com>
2022-04-19 20:42:19 +0900

    [ADD] 검색결과 Total 함수 추가, viewVisible 함수 추가

--- app/src/main/java/org/software/githubapiproject/adpater/SearchBindingAdapter.kt
+++ app/src/main/java/org/software/githubapiproject/adpater/SearchBindingAdapter.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -29,6 +30,21 @@ object SearchBindingAdapter {
     @JvmStatic
     @BindingAdapter("loading")
     fun loadingVisible(view : ProgressBar, type : Boolean){
+        if (type) view.visibility = View.VISIBLE
+        else view.visibility = View.GONE
+    }
+
+    @JvmStatic
+    @BindingAdapter("total","totalVisible")
+    fun totalFun(view:TextView, total : Int, type : Boolean){
+        view.text = "검색결과 : $total"
+        if (type) view.visibility = View.VISIBLE
+        else view.visibility = View.GONE
+    }
+
+    @JvmStatic
+    @BindingAdapter("viewVisible")
+    fun viewVisible(view: View , type : Boolean){
         if (type) view.visibility = View.VISIBLE
         else view.visibility = View.GONE
     }
