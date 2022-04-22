6932e4534e223184e5caa1689f3c8de9eb8af56a
KDY <rlaeodud9983@naver.com>
2022-04-22 16:16:15 +0900

    [UPDATE] PagingRepo 함수 추가

--- app/src/main/java/org/software/githubapiproject/repo/SearchUserRepo.kt
+++ app/src/main/java/org/software/githubapiproject/repo/SearchUserRepo.kt
@@ -1,14 +1,22 @@
 package org.software.githubapiproject.repo
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import androidx.paging.liveData
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
 import dagger.hilt.components.SingletonComponent
+import org.software.githubapiproject.page.MyPagingSource
 import org.software.githubapiproject.request.api.GitHubApi
 import javax.inject.Inject
 
 
-class SearchUserRepo @Inject constructor(private val api : GitHubApi){
-    suspend fun searchUser(user:String) = api.getSearchResponse(user)
-    suspend fun getUserInfo(user : String) = api.getUserInfo(user)
+class SearchUserRepo @Inject constructor(private val api: GitHubApi) {
+    fun searchUser(user: String) = Pager(
+        config = PagingConfig(pageSize = 5, maxSize = 25, enablePlaceholders = false),
+        pagingSourceFactory = { MyPagingSource(api, user) }).flow
+
+    suspend fun getUserInfo(user: String) = api.getUserInfo(user)
 }
